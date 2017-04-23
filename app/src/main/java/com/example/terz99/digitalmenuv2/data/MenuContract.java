38402efb469cb54e7952c22168d8266650e7f03b
Dushan Terzikj <dusanterzic87@gmail.com>
2017-04-23 23:37:08 +0200

    Add database helpers: MenuDbHelper.java and OrderDbHelper.java Add database contracts: MenuContract.java and OrderContract.java

---
+++ app/src/main/java/com/example/terz99/digitalmenuv2/data/MenuContract.java
@@ -0,0 +1,51 @@
+package com.example.terz99.digitalmenuv2.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by terz99 on 4/23/17.
+ */
+
+public class MenuContract {
+
+    // String which represents the authority of the content URI
+    public static final String CONTENT_AUTHORITY = "com.example.terz99.digitalmenuv2.data";
+    // URI which represents the scheme and the authority of the content URI
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    // The path of the table
+    public static final String PATH_MENU = "menu";
+
+    public static class MenuEntry implements BaseColumns{
+
+        // This string helps the getType() method in the content provider to return the type of the
+        // whole table
+        public static final String CONTENT_LIST_TYPE  = ContentResolver.CURSOR_DIR_BASE_TYPE
+                + "/" + CONTENT_AUTHORITY + "/" + PATH_MENU;
+
+        // This string helps the getType() method in the content provider to return the type of
+        // a single row
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+                + "/" + CONTENT_AUTHORITY + "/" + PATH_MENU + "/#";
+
+        // The main URI of the database content
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MENU)
+                .build();
+
+        // The name of the table. This name has to be the same as the path of the menu (PATH_MENU)
+        public static final String TABLE_NAME = "menu";
+
+
+        // The name of the category id column
+        public static final String COLUMN_CATEGORY_ID = "category_id";
+        // The name of the name column
+        public static final String COLUMN_NAME = "name";
+        // The name of the description column
+        public static final String COLUMN_DESCRIPTION = "description";
+        // The name of the price column
+        public static final String COLUMN_PRICE = "price";
+        // The name of the photo column
+        public static final String COLUMN_PHOTO_ID = "photo_id";
+    }
+}
---
+++ app/src/main/java/com/example/terz99/digitalmenuv2/data/OrderDbHelper.java
@@ -0,0 +1,61 @@
+package com.example.terz99.digitalmenuv2.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.terz99.digitalmenuv2.data.OrderContract.*;
+
+/**
+ * Created by terz99 on 4/23/17.
+ */
+
+public class OrderDbHelper extends SQLiteOpenHelper{
+
+    // The name of the order database
+    private static final String DATABASE_NAME = "order.db";
+    // The database version
+    private static int DATABASE_VERSION = 0;
+
+    /**
+     * Public constructor used to get a link from the database helper
+     * @param context is the Context where the database helper is created in
+     */
+    public OrderDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Overridden method which creates a table
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // SQL command to create a database of the menu
+        final String SQL_CREATE_TABLE = "CREATE TABLE " + OrderEntry.TABLE_NAME
+                + "("
+                + OrderEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + OrderEntry.COLUMN_NAME + " TEXT NOT NULL, "
+                + OrderEntry.COLUMN_DESCRIPTION + " TEXT NOT NULL, "
+                + OrderEntry.COLUMN_PRICE + " TEXT NOT NULL, "
+                + OrderEntry.COLUMN_QUANTITY + " INTEGER NOT NULL, "
+                + OrderEntry.COLUMN_PHOTO_ID + " INTEGER NOT NULL);";
+
+        // Execute the command via SQLiteDatabase
+        db.execSQL(SQL_CREATE_TABLE);
+    }
+
+    // Overridden method which deletes (drops) a database if exists and creates a new upgraded
+    // version
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        // SQL command to drop (delete) a table in the database
+        final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + OrderEntry.TABLE_NAME;
+
+        // Execute the command via SQLiteDatabase
+        db.execSQL(SQL_DELETE_TABLE);
+
+        // Create a new database (upgrade)
+        onCreate(db);
+    }
+}
